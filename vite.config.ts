a93a811a1977c5e39d4dc10f419342a1791ce223
hisho <hisho.web@gmail.com>
2022-05-19 00:56:36 +0900

    🎉 init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { join } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  root: './src',
+  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': join(__dirname, 'src'),
+    },
+  },
+})
